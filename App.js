2b3630b1704e36fc21df2873b4144e7888896b74
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-09-17 23:57:34 +0100

    feat: onboarding screens added

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Prompt_400Regular } from "@expo-google-fonts/prompt";
+import FirstPage from './screens/onboarding/FirstPage';
+import SecondPage from './screens/onboarding/SecondPage';
+import ThirdPage from './screens/onboarding/ThirdPage';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Prompt_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <View style={styles.loadingContainer} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! back to expo once again</Text>
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <FirstPage />
+        {/* <SecondPage />
+        <ThirdPage /> */}
+      </SafeAreaView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
---
+++ screens/onboarding/ThirdPage.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { useFonts, Prompt_400Regular } from "@expo-google-fonts/prompt";
+
+const ThirdPage = () => {
+  return (
+      <View>
+          <View style={styles.container}>
+              <Image source={require('../../assets/third-screen.png')} style={styles.image} />
+          </View>
+
+          <View style={styles.textContainer}>
+              <Text style={styles.onboardingHeadingText}>
+                  Easily find information on all governmental agencies. 
+              </Text>
+              <Text style={styles.text}>
+                  Effortlessly Access Comprehensive Government Agency Information! Our platform provides a convenient and user-friendly way to discover details about all governmental agencies.
+              </Text>
+          </View>
+
+          <View style={styles.buttonsContainer}>
+              <TouchableOpacity>
+                  <View style={styles.outlineButtonContainer}>
+                      <Text style={styles.outlineButtonText}>RETURN</Text>
+                  </View>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                  <View style={styles.buttonContainer}>
+                      <Text style={styles.buttonText}>GET STARTED</Text>
+                  </View>
+              </TouchableOpacity>
+          </View>
+
+          <View style={styles.signUpTexts}>
+              <Text style={styles.hurry}>Not in a hurry?</Text>
+              <Text style={styles.signUp}>SIGN UP</Text>
+          </View>
+      </View>
+  )
+}
+
+export default ThirdPage
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 'auto',
+        marginBottom: 15,
+    },
+    image: {
+        height: 500,
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        marginBottom: 10,
+    },
+    onboardingHeadingText: {
+        fontSize: 23,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: '#19686A',
+        fontFamily: 'Prompt_400Regular',
+    },
+    text: {
+        fontSize: 16,
+        marginBottom: 10,
+        lineHeight: 25,
+        fontFamily: 'Prompt_400Regular',
+    },
+    buttonsContainer: {
+        width: 320,
+        marginLeft: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    buttonContainer: {
+        width: 152,
+        paddingHorizontal: 20,
+        backgroundColor: '#19686A',
+        paddingVertical: 12,
+        borderRadius: 4,
+    },
+    buttonText: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#fff',
+        fontFamily: 'Prompt_400Regular',
+    },
+    outlineButtonContainer: {
+        width: 152,
+        paddingHorizontal: 20,
+        backgroundColor: 'white',
+        paddingVertical: 12,
+        borderRadius: 4,
+        borderWidth: 1.5,
+        borderColor: '#19686A',
+        borderStyle: 'solid',
+    },
+    outlineButtonText: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#19686A',
+        fontFamily: 'Prompt_400Regular',
+    },
+    signUpTexts: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 5,
+    },
+    hurry: {
+        fontWeight: 'bold',
+        fontFamily: 'Prompt_400Regular',
+    },
+    signUp: {
+        color: '#19686A',
+        fontWeight: 'bold',
+        fontFamily: 'Prompt_400Regular',
+    },
+})
